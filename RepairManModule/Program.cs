13d12d80ccdf0754f46da01401a3191b2a615f62
KhomenkoMac <m.khomenko03@gmail.com>
2021-02-12 23:08:45 +0200

    Added: Log in form for repairman + the form was set to run as first + minimal design

--- RepairManModule/Program.cs
+++ RepairManModule/Program.cs
@@ -16,7 +16,7 @@ namespace RepairManModule
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new RepairManLogInForm());
         }
     }
 }
---
+++ RepairManModule/RepairManLogInForm.Designer.cs
@@ -0,0 +1,107 @@
+﻿
+namespace RepairManModule
+{
+	partial class RepairManLogInForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RepairManLogInForm));
+			this.LogInButton = new System.Windows.Forms.Button();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// LogInButton
+			// 
+			this.LogInButton.Location = new System.Drawing.Point(344, 160);
+			this.LogInButton.Margin = new System.Windows.Forms.Padding(7, 7, 7, 7);
+			this.LogInButton.Name = "LogInButton";
+			this.LogInButton.Size = new System.Drawing.Size(165, 69);
+			this.LogInButton.TabIndex = 0;
+			this.LogInButton.Text = "Log In";
+			this.LogInButton.UseVisualStyleBackColor = true;
+			this.LogInButton.Click += new System.EventHandler(this.LogInButton_Click);
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+			this.pictureBox1.Location = new System.Drawing.Point(12, 53);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(234, 222);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox1.TabIndex = 1;
+			this.pictureBox1.TabStop = false;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(7, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(186, 29);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "Happy PC Corp.";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(339, 106);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(170, 29);
+			this.label2.TabIndex = 3;
+			this.label2.Text = "Log in, please.";
+			// 
+			// RepairManLogInForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(601, 299);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.LogInButton);
+			this.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Margin = new System.Windows.Forms.Padding(7, 7, 7, 7);
+			this.Name = "RepairManLogInForm";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Log In";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button LogInButton;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+	}
+}
