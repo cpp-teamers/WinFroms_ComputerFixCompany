7f6e973ca29e82251151e1402bf842d7915047ec
KhomenkoMac <m.khomenko03@gmail.com>
2021-02-15 09:12:26 +0200

    Designed forms: fixed size

--- RepairManModule/RepairManLogInForm.Designer.cs
+++ RepairManModule/RepairManLogInForm.Designer.cs
@@ -110,6 +110,7 @@ namespace RepairManModule
 			this.Controls.Add(this.pictureBox1);
 			this.Controls.Add(this.LogInButton);
 			this.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
 			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
 			this.Margin = new System.Windows.Forms.Padding(7);
 			this.MaximizeBox = false;
